69c913a8f60b2a50b4559bf5b35273ef74e1b24d
havenothinghavesomething <150053613+havenothinghavesomething@users.noreply.github.com>
2023-11-07 01:00:51 +0000

    - User query functions added

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 print("\nWelcome to ABS 1!")
-print("Hello " * 5 + "\n\n")
-print("What's your name?\n")
-yourName = input()
-print("\nHello " + yourName)
-nameFile = open('.experimentalOutputs/nameFile', 'w')
-nameFile.write(yourName)
-print("Your name has been recorded")
+print("Hello " * 5)
+nameFilename = ".experimentalOutputs/nameFile"
+def userQueryYesNo(queryString):
+  print(queryString + " (Y/n)")
+  response = input()
+  if response == "Y":
+    return True
+  else:
+    return 0
+def userQueryForString(queryString):
+  print(queryString)
+  queryResponse = input()
+  return queryResponse
+def getName(): 
+  if userQueryYesNo("Would you like to enter your name?"):
+    yourName = userQueryForString("What's your name?")
+    nameFile = open(nameFilename, "a")
+    nameFile.write("\n"+yourName)
+    print("Your name has been recorded")
+    nameFile.close()
+def printNames():
+  nameFile = open(nameFilename)
+  print("\nThe names that have been recorded are:")
+  print()
+  allNames = nameFile.readlines()
+  for x in allNames:
+    #print(x)
+    print(x.removesuffix("\n"))
+while 1 == 1:
+  print()
+  getName()
+  printNames()
